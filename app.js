7c21b195843a0f86a103c6befbaa168b5b47b635
IdirX69 <adil.idirr@gmail.com>
2022-11-07 10:55:33 +0100

    expresse users quest

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+
+const port = process.env.APP_PORT ?? 5000;
+
+const welcome = (req, res) => {
+  res.send("Welcome to my favourite movie list");
+};
+
+app.get("/", welcome);
+
+const usersHandlers = require("./usersHandlers");
+
+app.get("/api/users", usersHandlers.getUsers);
+app.get("/api/users/:id", usersHandlers.getUsersById);
+
+app.listen(port, (err) => {
+  if (err) {
+    console.error("Something bad happened");
+  } else {
+    console.log(`Server is listening on ${port}`);
+  }
+});
